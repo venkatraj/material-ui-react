529cbc52c4a3d0c4e0236e04fe4383514e530dc5
Venkat <venkat@codinggeek.com>
2020-08-12 12:51:05 +0530

    buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button variant="outlined">Default Outline</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button color="secondary">Text Button With Secondary Color</Button>
+      <Button color="primary" disabled fullWidth>
+        FullWidth Disabled Primary Button
+      </Button>
+    </div>
+  );
 }
 
 export default App;
